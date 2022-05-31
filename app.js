c35772016a679eee6069c2e03b11ba753ce7e099
Simon <39000980+simschwab@users.noreply.github.com>
2022-05-31 15:45:29 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const app = require("express")();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("", (req, res) => {
+    res.send("Hello DevOps Course, first Deployment on Heroku");
+});
+
+app.listen(PORT, () => {
+    console.log(`App up at port ${PORT}`)
+});
+
